68ff93cae634741887b9e4838e8a5fe09be5465a
ShridharPawar <spaw0004@student.monash.edu>
2020-09-14 00:31:01 +1000

    lastatnight

---
+++ Plaboo/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Azure;
+using Plaboo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Plaboo.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public ActionResult LogOn()
+        {
+            LogOnViewModel model = new LogOnViewModel();
+
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public ActionResult LogOn(LogOnViewModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                //FormsAuthentication.SetAuthCookie(model.UserName, false);
+                //return RedirectToAction("Index", "Home");
+                if (model.UserName == CloudConfigurationManager.GetSetting("UName") && model.Password == CloudConfigurationManager.GetSetting("UPw"))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+                    return RedirectToAction("Index", "Home");
+
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Incorrect username or password");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult LogOff()
+        {
+            Request.Cookies.Remove("UserName");
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- Plaboo/Controllers/HomeController.cs
+++ Plaboo/Controllers/HomeController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 
 namespace Plaboo.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         public ActionResult RecyclingSuggestions()

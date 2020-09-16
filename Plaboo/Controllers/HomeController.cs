d2ef4a8c4f091f0507f20baf9f8dde37560a8590
ShridharPawar <spaw0004@student.monash.edu>
2020-09-17 00:59:42 +1000

    Finally

--- Plaboo/Controllers/HomeController.cs
+++ Plaboo/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Web.Mvc;
 
 namespace Plaboo.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
         //this is the HTTP get method to retreive the 'Recycling Suggestions' page

98b572fdba9211aa80ef7b87cf8331e12676db18
ShridharPawar <spaw0004@student.monash.edu>
2020-09-25 22:09:02 +1000

    aftermap

--- Plaboo/App_Start/BundleConfig.cs
+++ Plaboo/App_Start/BundleConfig.cs
@@ -22,6 +22,9 @@ namespace Plaboo
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/mapbox").Include(
+                       "~/Scripts/location.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
